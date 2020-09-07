168329837a39400637011c6c84f06b757c51f4e4
Peter1966-r <68447297+Peter1966-r@users.noreply.github.com>
2020-09-07 15:13:58 -0300

    Delete Histogramtotal.R

--- Histogramtotal.R
+++
@@ -1,5 +0,0 @@
-StepsTotalUnion <- aggregate(steps ~ date, data = new_activity, sum, na.rm = TRUE)
-hist(StepsTotalUnion$steps, main = paste("Total Steps Each Day"), col="blue", xlab="Number of Steps")
-#Create Histogram to show difference. 
-hist(steps_by_day$steps, main = paste("Total Steps Each Day"), col="green", xlab="Number of Steps", add=T)
-legend("topright", c("Imputed", "Non-imputed"), col=c("blue", "green"), lwd=10)
